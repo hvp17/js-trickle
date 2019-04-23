25f6d88f91e21866123169e46f053f81036a747c
Thomas Smith <thomassmith@Thomass-MacBook-Pro-3.local>
2019-04-23 12:33:03 +0200

    bla

--- apis/api-signup.php
+++ apis/api-signup.php
@@ -2,7 +2,7 @@
 
 ///////    SYNTAX ERROR CHECK    ////////////
 error_reporting(E_ALL);
-ini_set('display_errors','On');
+ini_set('display_errors', 1);
 ///////    END SYNTAX ERROR CHECK    ////////////
 
 
@@ -12,7 +12,6 @@ ini_set('display_errors','On');
       'cost'=> 5,
   ];
 $hashed_password =  password_hash($password, PASSWORD_DEFAULT, $options);
-
   require_once '../db.php';
   try{
     $iBlockedDate = time();
@@ -25,7 +24,7 @@ $hashed_password =  password_hash($password, PASSWORD_DEFAULT, $options);
     $sQuery->bindValue(':bBlocked', 0);
     $sQuery->bindValue(':iBlockedDate', $iBlockedDate);
     $sQuery->execute();
-    echo 'hi';
+    
     if( $sQuery->rowCount() ){
       echo '{"status":1, "message":"success"}';
       exit;
