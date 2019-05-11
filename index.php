ed67b7a2a8a091cf1128bfd73af8e8407a5bfaaf
Thomas Smith <thomassmith@Thomass-MacBook-Pro-3.local>
2019-05-11 11:22:50 +0200

    index

--- index.php
+++ index.php
@@ -13,14 +13,8 @@ require_once "components/top.php";
               <div class="row mb-3">
                 <div class="col-md-9">
                   <div class="row">
-                    <div class="col-md-6 mb-3 mb-md-0">
-                      <input type="text" class="mr-3 form-control border-0 px-4" placeholder="job title, keywords or company name ">
-                    </div>
-                    <div class="col-md-6 mb-3 mb-md-0">
-                      <div class="input-wrap">
-                        <span class="icon icon-room"></span>
-                      <input type="text" class="form-control form-control-block search-input  border-0 px-4" id="autocomplete" placeholder="city, province or region" onFocus="geolocate()">
-                      </div>
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <input type="text" class="mr-3 form-control border-0 px-4" placeholder="Search...">
                     </div>
                   </div>
                 </div>
@@ -30,7 +24,7 @@ require_once "components/top.php";
               </div>
               <div class="row">
                 <div class="col-md-12">
-                  <p class="small">or browse by category: <a href="#" class="category">Category #1</a> <a href="#" class="category">Category #2</a></p>
+                  <p class="small">or browse by tag: <a href="/tags.php" class="category">Tags</a></p>
                 </div>
               </div>
 
