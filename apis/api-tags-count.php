cc85714f1e4b0599af6d4dec8898265398b53865
tsmithdk <tsmithdk@gmail.com>
2019-05-11 16:54:14 +0200

    all tags page with count is done

---
+++ apis/api-tags-count.php
@@ -0,0 +1,16 @@
+  <?php
+
+require_once __DIR__.'/../db.php';
+try{
+  $sQuery = $db->prepare('SELECT tags.name, count(tags.name) as numberOfQuestions FROM tags
+  INNER JOIN questions_tags ON tags.id = questions_tags.tags_fk
+  group by tags.name
+  ');
+  $sQuery->execute();
+  $aTags = $sQuery->fetchAll();
+
+  echo json_encode($aTags);
+
+}catch(PDOException $exception){
+  echo '{"status":0, "message":"Indlæg kan ikke vises"}';
+}
--- tags.php
+++ tags.php
@@ -13,63 +13,7 @@ require_once "components/top.php";
 
     <div class="site-section">
       <div class="container">
-        <div class="row">
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-calculator mb-3 text-primary"></span>
-              <h2>Accounting / Finanace</h2>
-              <span class="counting">10,391</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-wrench mb-3 text-primary"></span>
-              <h2>Automotive Jobs</h2>
-              <span class="counting">192</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-worker mb-3 text-primary"></span>
-              <h2>Construction / Facilities</h2>
-              <span class="counting">1,021</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-telecommunications mb-3 text-primary"></span>
-              <h2>Telecommunications</h2>
-              <span class="counting">1,219</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-stethoscope mb-3 text-primary"></span>
-              <h2>Healthcare</h2>
-              <span class="counting">482</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-computer-graphic mb-3 text-primary"></span>
-              <h2>Design, Art &amp; Multimedia</h2>
-              <span class="counting">5,409</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-trolley mb-3 text-primary"></span>
-              <h2>Transportation &amp; Logistics</h2>
-              <span class="counting">291</span>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <a href="#" class="h-100 feature-item">
-              <span class="d-block icon flaticon-restaurant mb-3 text-primary"></span>
-              <h2>Restaurant / Food Service</h2>
-              <span class="counting">329</span>
-            </a>
-          </div>
+        <div class="row" id="allTags">
         </div>
     </div>
  
@@ -81,6 +25,7 @@ require_once "components/top.php";
     
       
     <?php
+    $sScript ='tags.js';
     require_once "components/bottom.php";
     ?>
     
